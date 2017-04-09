18802e7516a78d83f70b7b8b2aa2d5b3537ea8e1
Melissa Cat <missmel@4o4.nl>
2017-04-09 13:58:38 +0200

    Formatting

--- Bubblegum/MessageHandlers/ScripthookVersion.cs
+++ Bubblegum/MessageHandlers/ScripthookVersion.cs
@@ -10,32 +10,33 @@ using System.Threading.Tasks;
 
 namespace Bubblegum.MessageHandlers
 {
-    class ScripthookVersion : BotPlugin
-    {
-        public const string ScripthookUrl = "http://www.dev-c.com/gtav/scripthookv/";
+	class ScripthookVersion : BotPlugin
+	{
+		public const string ScripthookUrl = "http://www.dev-c.com/gtav/scripthookv/";
 
-        public override string HandleMessage(Message msg)
-        {
-            var match = Regex.Match(msg.Text, @"\.(?i)([Ss]cripthook)");
+		public override string HandleMessage(Message msg)
+		{
+			var match = Regex.Match(msg.Text, @"\.(?i)([Ss]cripthook)");
 
-            if (!match.Success)
-            {
-                return null;
-            }
+			if (!match.Success) {
+				return null;
+			}
 
-            var wc = new WebClient();
-            wc.Proxy = null;
-            wc.Headers[HttpRequestHeader.UserAgent] = "Bubblegum / 2.0";
+			var wc = new WebClient();
+			wc.Proxy = null;
+			wc.Headers[HttpRequestHeader.UserAgent] = "Bubblegum / 2.0";
 
-            HtmlDocument doc = new HtmlDocument();
-            doc.LoadHtml(wc.DownloadString(ScripthookUrl));
+			HtmlDocument doc = new HtmlDocument();
+			doc.LoadHtml(wc.DownloadString(ScripthookUrl));
 
-            var releasedDateNode = doc.DocumentNode.SelectSingleNode("//*[@id=\"tfhover\"]/tbody/tr[1]/td");
-            var currentVersionNode = doc.DocumentNode.SelectSingleNode("//*[@id=\"tfhover\"]/tbody/tr[2]/td");
+			var releasedDateNode = doc.DocumentNode.SelectSingleNode("//*[@id=\"tfhover\"]/tbody/tr[1]/td");
+			var currentVersionNode = doc.DocumentNode.SelectSingleNode("//*[@id=\"tfhover\"]/tbody/tr[2]/td");
 
-            if (releasedDateNode == null || currentVersionNode == null) return null;
+			if (releasedDateNode == null || currentVersionNode == null) {
+				return null;
+			}
 
-            return $"Current scripthook version is {currentVersionNode.InnerText} released at {releasedDateNode.InnerText}";
-        }
-    }
+			return $"Current scripthook version is {currentVersionNode.InnerText} released at {releasedDateNode.InnerText}";
+		}
+	}
 }
